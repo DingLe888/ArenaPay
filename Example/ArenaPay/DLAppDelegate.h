6519717c35f4b5a18e295a01dd705232e363432b
dingle <18911755085@163.com>
2017-10-26 09:28:48 +0800

    Initial commit

---
+++ Example/ArenaPay/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  ArenaPay
+//
+//  Created by 18911755085@163.com on 10/26/2017.
+//  Copyright (c) 2017 18911755085@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ArenaPay/DLViewController.h
@@ -0,0 +1,13 @@
+//
+//  DLViewController.h
+//  ArenaPay
+//
+//  Created by 18911755085@163.com on 10/26/2017.
+//  Copyright (c) 2017 18911755085@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DLViewController : UIViewController
+
+@end
